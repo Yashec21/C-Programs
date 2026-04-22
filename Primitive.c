e0b11f633d30eeda8e9ab8634dd8ae81e5543bc4
Yashec21 <yashpatilec21@gmail.com>
2026-04-23 00:51:48 +0530

    Add files via upload

---
+++ Primitive.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch ='A';
+    int i = 11;
+    float f=99.99f;
+    double d=78.67345;
+
+    printf("R values of all variable");
+    printf("%c\n",ch);
+    printf("%d\n",i);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+
+
+    return 0;
+}
